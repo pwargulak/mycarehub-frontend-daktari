3bc5fe07a0926d2a818f70433d540c6ffd55b3e9
Byron Kimani <byronkimani@gmail.com>
2023-02-27 09:41:28 +0300

    chore: show bottom nav bar on communities
    - show bottom nav bar on communities

--- lib/presentation/communities/pages/communities_list_page.dart
+++ lib/presentation/communities/pages/communities_list_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:prohealth360_daktari/domain/core/value_objects/app_strings.dart';
 import 'package:prohealth360_daktari/presentation/core/app_bar/custom_app_bar.dart';
+import 'package:prohealth360_daktari/presentation/core/bottom_nav/bottom_nav_bar.dart';
+import 'package:prohealth360_daktari/presentation/core/bottom_nav/bottom_nav_items.dart';
 
 class CommunitiesListPage extends StatelessWidget {
   const CommunitiesListPage({super.key});
@@ -8,8 +10,14 @@ class CommunitiesListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const CustomAppBar(title: conversationsTitle),
+      appBar: const CustomAppBar(
+        title: conversationsTitle,
+        showBackButton: false,
+      ),
       body: Column(),
+      bottomNavigationBar: BottomNavBar(
+        bottomNavIndex: BottomNavIndex.communities.index,
+      ),
     );
   }
 }
