87d8287cbed79c65a84b67f8d95c1adf66fe73c4
Eugene Odhiambo <eugenemrlens@gmail.com>
2023-03-14 17:13:06 +0300

    chore: conversations zero state

--- lib/presentation/communities/pages/room_list_page.dart
+++ lib/presentation/communities/pages/room_list_page.dart
@@ -1,8 +1,10 @@
 import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
+import 'package:prohealth360_daktari/application/core/theme/app_themes.dart';
 import 'package:prohealth360_daktari/application/redux/actions/flags/app_flags.dart';
 import 'package:prohealth360_daktari/application/redux/states/app_state.dart';
 import 'package:prohealth360_daktari/application/redux/view_models/communities/room_list_view_model.dart';
+import 'package:prohealth360_daktari/domain/core/value_objects/app_asset_strings.dart';
 import 'package:prohealth360_daktari/domain/core/value_objects/app_strings.dart';
 import 'package:prohealth360_daktari/domain/core/value_objects/app_widget_keys.dart';
 import 'package:prohealth360_daktari/presentation/communities/pages/room_list_item_widget.dart';
@@ -10,6 +12,7 @@ import 'package:prohealth360_daktari/presentation/core/app_bar/custom_app_bar.da
 import 'package:prohealth360_daktari/presentation/core/bottom_nav/bottom_nav_bar.dart';
 import 'package:prohealth360_daktari/presentation/core/bottom_nav/bottom_nav_items.dart';
 import 'package:prohealth360_daktari/presentation/router/routes.dart';
+import 'package:sghi_core/afya_moja_core/afya_moja_core.dart';
 import 'package:sghi_core/communities/models/room.dart';
 
 class RoomListPage extends StatelessWidget {
@@ -55,25 +58,22 @@ class RoomListPage extends StatelessWidget {
                   ];
 
                   if (allRooms.isEmpty) {
-                    return Center(
-                      child: Column(
-                        children: <Widget>[
-                          const Text(
-                            roomsZeroStateDescriptionText,
-                            textAlign: TextAlign.center,
+                    return GenericErrorWidget(
+                      actionText: createARoom,
+                      actionKey: emptyChatsNewRoomKey,
+                      headerIconSvgUrl: noChatImage,
+                      messageTitle: noConversationsAvailable,
+                      messageBody: <TextSpan>[
+                        TextSpan(
+                          text: noConversationsDescription,
+                          style: normalSize16Text(
+                            AppColors.greyTextColor,
                           ),
-                          const SizedBox(height: 10),
-                          Center(
-                            child: TextButton(
-                              key: emptyChatsNewRoomKey,
-                              onPressed: () => Navigator.pushNamed(
-                                context,
-                                AppRoutes.createRoomPage,
-                              ),
-                              child: const Text(createARoom),
-                            ),
-                          ),
-                        ],
+                        ),
+                      ],
+                      recoverCallback: () => Navigator.pushNamed(
+                        context,
+                        AppRoutes.createRoomPage,
                       ),
                     );
                   }
